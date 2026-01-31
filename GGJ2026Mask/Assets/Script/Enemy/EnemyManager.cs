ce001a103faf5b7cbe85764a85f5d5b01b229f98
Geoffrey Lawson <geoffrey.lawson0@gmail.com>
2026-01-31 21:35:26 +0900

    Spawn Enemies

---
+++ GGJ2026Mask/Assets/Script/Enemy/EnemyManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] prefabs;
+    private Dictionary<int,GameObject[]> spawns = new Dictionary<int, GameObject[]>();
+
+    void Start()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            var currSpawns = GameObject.FindGameObjectsWithTag("spawn" + i);
+            spawns.Add(i, currSpawns);
+
+            var currSpawn = currSpawns[Random.Range(0, currSpawns.Length)];
+            var enemy = Instantiate(prefabs[Random.Range(0, prefabs.Length)], currSpawn.transform.position, Quaternion.identity);
+            enemy.AddComponent<Enemy>().setController(this);
+            enemy.AddComponent<VisibleCheck>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
