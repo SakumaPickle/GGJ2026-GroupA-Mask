1800e24bab748dd4cdfe1ed5780170c038d06acd
KazuyaSakuma <sakuma@pickle.ne.jp>
2026-01-31 22:15:32 +0900

    日本語フォント、Tipsの追加、音量設定ダイアログ周りの整理

--- GGJ2026Mask/Assets/Script/Title/Title.cs
+++ GGJ2026Mask/Assets/Script/Title/Title.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 public class Title : MonoBehaviour
 {
+	[SerializeField] private GameObject settingDialog;
+	[SerializeField] private GameObject Tips;
+
 	void Start()
 	{
 		TransitFader.Instance.FadeIn().Forget();
@@ -14,5 +17,17 @@ public class Title : MonoBehaviour
 		SoundManager.Instance.StopBGM();
 		SoundManager.Instance.PlaySE(SoundManager.Se.Decision);
 		TransitFader.Instance.FadeOutAsync("TestScene").Forget();
+	}
+
+	public void OnClicSettingButton()
+	{
+		SoundManager.Instance.PlaySE(SoundManager.Se.Decision);
+		settingDialog.SetActive(true);
+	}
+
+	public void OnClickTipsButton()
+	{
+		SoundManager.Instance.PlaySE(SoundManager.Se.Decision);
+		Tips.SetActive(true);
 	}
 }
